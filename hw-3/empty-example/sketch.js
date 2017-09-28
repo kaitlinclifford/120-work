bec9cf6bb2949194ca6934a45136419e0459d535
Kaitlin Clifford <kaitlin.clifford@umontana.edu>
2017-09-27 21:37:28 -0600

    I'm hoping this works the way I want

---
+++ hw-3/empty-example/sketch.js
@@ -0,0 +1,17 @@
+// create the setup() function
+function setup() {
+
+// create a canvas that is 700px X 500px
+  createCanvas( 700,500 );
+
+// create a background with the color 'magenta'
+  background( 'magenta' );
+}
+
+// create a draw() function
+function draw() {
+
+  //create text string with 'Hello World!' inside
+  text( 'Hello World!', 300, 250 );
+
+}
