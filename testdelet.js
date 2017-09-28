fa6776170d597263f3c0d684523cd4b6c00a6f32
Kaitlin Clifford <kaitlin.clifford@umontana.edu>
2017-09-27 18:34:33 -0600

    test delete

---
+++ testdelet.js
@@ -0,0 +1,15 @@
+// create setup() function
+function setup() {
+
+  // create a canvas that is 600px X 400px
+  createCanvas( 700, 500 );
+
+  // create a background() in magenta
+  background( 'magenta' );
+}
+// create a draw() function
+function draw() {
+
+  // create text string 'Hello World!'
+  text( 'Hello World!', 300, 200 );
+}
